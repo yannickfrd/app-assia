66d2b0a5880210ad01695d5985ccd5349f51e6de
RomMad <romain.madelaine@gmail.com>
2020-02-13 10:14:51 +0100

    Modif bugs divers d'affichage + accessibilité page login

--- src/Form/Support/SupportPersonType.php
+++ src/Form/Support/SupportPersonType.php
@@ -16,25 +16,32 @@ class SupportPersonType extends AbstractType
     {
         $builder
             ->add("startDate", DateType::class, [
+                "label_attr" => ["class" => "sr-only"],
                 "widget" => "single_text",
                 "attr" => [
-                    "class" => "w-max-180",
+                    "class" => "w-max-165",
                 ],
                 "required" => true
             ])
             ->add("endDate", DateType::class, [
+                "label_attr" => ["class" => "sr-only"],
                 "widget" => "single_text",
                 "attr" => [
-                    "class" => "w-max-180",
+                    "class" => "w-max-165",
                 ],
                 "required" => false
             ])
             ->add("status", ChoiceType::class, [
+                "label_attr" => ["class" => "sr-only"],
                 "choices" => Choices::getChoices(SupportPerson::STATUS),
+                "attr" => [
+                    "class" => "w-min-150"
+                ],
                 "placeholder" => "-- Select --",
                 "required" => true
             ])
             ->add("comment", null, [
+                "label_attr" => ["class" => "sr-only"],
                 "attr" => [
                     "rows" => 1,
                 ]
