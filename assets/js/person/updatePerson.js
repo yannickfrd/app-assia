8a243f1b9ce5412e2eee1aefcc3b2b413053f0b6
RomMad <romain.madelaine@gmail.com>
2020-09-23 13:37:18 +0200

    Correction updatePerson : ajout conditionnalité

--- assets/js/person/updatePerson.js
+++ assets/js/person/updatePerson.js
@@ -11,14 +11,13 @@ export default class UpdatePerson {
     constructor() {
         this.ajaxRequest = new AjaxRequest()
         this.personElt = document.querySelector('form[name=person]')
-        this.updatePersonBtn = document.getElementById('updatePerson')
-        this.url = this.updatePersonBtn.getAttribute('data-url')
+        this.updatePersonBtnElt = document.getElementById('updatePerson')
         this.loader = new Loader()
         this.init()
     }
 
     init() {
-        let validationPerson = new ValidationPerson(
+        const validationPerson = new ValidationPerson(
             'person_lastname',
             'person_firstname',
             'person_birthdate',
@@ -26,19 +25,22 @@ export default class UpdatePerson {
             'person_email'
         )
 
-        this.updatePersonBtn.addEventListener('click', e => {
-            e.preventDefault()
-            if (validationPerson.getNbErrors() === 0) {
-                this.loader.on()
-                let formData = new FormData(this.personElt)
-                let formToString = new URLSearchParams(formData).toString()
-                this.ajaxRequest.init('POST', this.url, this.response.bind(this), true, formToString)
-            }
-        })
+        if (this.updatePersonBtnElt) {
+            this.updatePersonBtnElt.addEventListener('click', e => {
+                e.preventDefault()
+                if (validationPerson.getNbErrors() === 0) {
+                    this.loader.on()
+                    const formData = new FormData(this.personElt)
+                    const formToString = new URLSearchParams(formData).toString()
+                    const url = this.updatePersonBtnElt.getAttribute('data-url')
+                    this.ajaxRequest.init('POST', url, this.response.bind(this), true, formToString)
+                }
+            })
+        }
     }
 
     response(data) {
-        let dataJSON = JSON.parse(data)
+        const dataJSON = JSON.parse(data)
 
         if (dataJSON.code === 200) {
             if (dataJSON.alert === 'success') {
