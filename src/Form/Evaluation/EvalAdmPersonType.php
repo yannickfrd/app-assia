35ec50039d2695f34b241d9a9afca1a5c47deadb
RomMad <romain.madelaine@gmail.com>
2022-04-29 16:34:49 +0200

    Remove Country Entity and use CountryType

--- src/Form/Evaluation/EvalAdmPersonType.php
+++ src/Form/Evaluation/EvalAdmPersonType.php
@@ -2,28 +2,19 @@
 
 namespace App\Form\Evaluation;
 
-use App\Entity\Evaluation\Country;
 use App\Entity\Evaluation\EvalAdmPerson;
 use App\Entity\Organization\Service;
 use App\Form\Utils\Choices;
 use App\Form\Utils\EvaluationChoices;
-use App\Repository\Evaluation\CountryRepository;
-use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class EvalAdmPersonType extends AbstractType
 {
-    private $countryRepo;
-
-    public function __construct(CountryRepository $countryRepo)
-    {
-        $this->countryRepo = $countryRepo;
-    }
-
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         /** @var Service */
@@ -108,24 +99,11 @@ class EvalAdmPersonType extends AbstractType
             ->add('ofpraRegistrationId', null, [
                 'attr' => ['data-mask-type' => 'number'],
             ])
-            ->add('_country', ChoiceType::class, [
-                'choices' => $this->getCountryChoices(),
-                'choice_label' => 'name',
-                'placeholder' => 'placeholder.select',
-                'mapped' => false,
-                'required' => false,
-            ])
+            ->add('_country', CountryType::class)
         ;
 
         if (!$builder->get('nationality')->getData()) {
             $builder->get('nationality')->setData(EvalAdmPerson::NATIONALITY_OUTSIDE_EU);
         }
-    }
-
-    private function getCountryChoices(): array
-    {
-        $cache = new FilesystemAdapter($_SERVER['DB_DATABASE_NAME']);
-
-        return $cache->get(Country::CACHE_COUNTRY_ALL_KEY, fn () => $this->countryRepo->findAll());
     }
 }
