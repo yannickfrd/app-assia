675fc5b05d60e28a4302d3f6f68b3daa14c8a349
RomMad <romain.madelaine@gmail.com>
2022-05-11 16:36:53 +0200

    Edit and refactor end-to-end tests for feature Note

--- tests/EndToEnd/NoteEndToEndTest.php
+++ tests/EndToEnd/NoteEndToEndTest.php
@@ -3,14 +3,24 @@
 namespace App\Tests\EndToEnd;
 
 use App\Tests\EndToEnd\Traits\AppPantherTestTrait;
-use Facebook\WebDriver\WebDriverBy;
 use Symfony\Component\Panther\Client;
-use Symfony\Component\Panther\DomCrawler\Crawler;
 use Symfony\Component\Panther\PantherTestCase;
 
 class NoteEndToEndTest extends PantherTestCase
 {
     use AppPantherTestTrait;
+
+    public const CONTAINER_NOTES = '#container-notes';
+    public const BUTTON_NEW_NOTE = 'button[data-action="new_note"]';
+    public const MODAL_BUTTON_SAVE = 'button[data-action="save"]';
+    public const MODAL_BUTTON_CLOSE = 'button[data-action="close"]';
+    public const FORM_NOTE = 'form[name="note"]';
+
+    public const MSG_FLASH = '#js-msg-flash';
+    public const BUTTON_CLOSE_MSG = '#btn-close-msg';
+    public const ALERT_SUCCESS = '.alert.alert-success';
+    public const ALERT_WARNING = '.alert.alert-warning';
+    public const ALERT_DANGER = '.alert.alert-danger';
 
     protected Client $client;
 
@@ -23,162 +33,151 @@ class NoteEndToEndTest extends PantherTestCase
     {
         $this->client = $this->loginUser();
 
-        /** @var Crawler */
-        $crawler = $this->client->request('GET', '/support/1/show');
+        $this->client->request('GET', '/support/1/show');
 
         $this->assertSelectorTextContains('h1', 'Suivi');
 
-        $crawler = $this->goToNotesPage($crawler);
-        $crawler = $this->failToCreateNote($crawler);
-//         $crawler = $this->createNote($crawler);
-        $crawler = $this->editNote($crawler);
-        $crawler = $this->deleteNote($crawler);
-        $this->restoreCardNote($crawler);
+        $this->showNotesPage();
+        $this->failToCreateNote();
+        $this->createNote();
+        $this->editNote();
+        $this->deleteNoteByModal();
+        $this->restoreCardNote();
+        $this->deleteNoteByTable();
         $this->restoreTableNote();
 
         $this->client->quit();
     }
 
-    private function goToNotesPage(Crawler $crawler): Crawler
+    private function showNotesPage(): void
     {
         $this->outputMsg('Show the notes page');
 
-        $link = $crawler->filter('#support-notes')->link();
+        $this->clickElement('#support-notes');
 
-        /** @var Crawler */
-        $crawler = $this->client->click($link);
-
-        $this->client->waitForVisibility('#container-notes');
+        $this->client->waitFor(self::CONTAINER_NOTES);
         $this->assertSelectorTextContains('h1', 'Notes sociales');
-
-        return $crawler;
     }
 
-    private function failToCreateNote(Crawler $crawler): Crawler
+    private function failToCreateNote(): void
     {
         $this->outputMsg('Fail to create a note');
 
-        $form = $crawler->filter('button[data-action="new_note"]')->click();
-        sleep(1); //pop-up effect
+        $this->clickElement(self::BUTTON_NEW_NOTE);
 
-        $form = $crawler->filter('button[data-action="save"]')->form([
-            'note[title]' => $this->faker->sentence(mt_rand(5, 10), true),
-        ]);
-
-        /** @var Crawler */
-        $crawler = $this->client->submit($form);
-
-        $this->client->waitForVisibility('#js-msg-flash');
-        $this->assertSelectorExists('#js-msg-flash.alert.alert-danger');
-
-        $crawler->selectButton('btn-close-msg')->click();
-        $crawler->filter('button[data-action="close"]')->click();
+        $this->client->waitFor(self::FORM_NOTE);
         sleep(1);
+        $this->client
+            ->getCrawler()
+            ->filter(self::FORM_NOTE)
+            ->form([
+                'note[title]' => $this->faker->sentence(mt_rand(5, 10), true),
+            ])
+        ;
 
-        return $crawler;
+        $this->clickElement(self::MODAL_BUTTON_SAVE);
+
+        $this->client->waitFor(self::MSG_FLASH);
+        $this->assertSelectorExists(self::MSG_FLASH.self::ALERT_DANGER);
+
+        $this->clickElement(self::BUTTON_CLOSE_MSG);
+        $this->clickElement(self::MODAL_BUTTON_CLOSE);
     }
 
-    private function createNote(Crawler $crawler): Crawler
+    private function createNote(): void
     {
         $this->outputMsg('Create a note');
 
-        $this->client->waitForVisibility('#js-new-note');
-        $form = $crawler->selectButton('js-new-note')->click();
-        sleep(1); //pop-up effect
+        $this->clickElement(self::BUTTON_NEW_NOTE);
 
-        $this->client->executeScript('document.getElementById("editor").click()');
-        $this->client->getKeyboard()->sendKeys('test');
-        $this->client->executeScript('navigator.clipboard.writeText("This text is now in the clipboard");');
-        $this->client->executeScript('document.execCommand("paste")');
-        $this->client->executeScript('document.getElementById("editor").innerHtml = "test"');
-
-        $form = $crawler->filter('button[data-action="save"]')->form([
-            'note[title]' => $this->faker->sentence(mt_rand(5, 10), true),
-            'note[editor]' => join('. ', $this->faker->paragraphs(mt_rand(1, 2))),
-        ]);
-
-        /** @var Crawler */
-        $crawler = $this->client->submit($form);
-
-        $this->client->waitForVisibility('#js-msg-flash');
-        $this->assertSelectorExists('#js-msg-flash.alert.alert-success');
-
-        $crawler->selectButton('btn-close-msg')->click();
-        $crawler->filter('button[data-action="close"]')->click();
+        $this->client->waitFor(self::FORM_NOTE);
         sleep(1);
+        $this->client
+            ->getCrawler()
+            ->filter(self::FORM_NOTE)
+            ->form([
+                'note[title]' => $this->faker->sentence(mt_rand(5, 10), true),
+                'note[editor]' => join('. ', $this->faker->paragraphs(mt_rand(1, 2))),
+                'note[tags]' => [1, 2],
+            ])
+        ;
 
-        return $crawler;
+        $this->clickElement(self::MODAL_BUTTON_SAVE);
+
+        $this->client->waitFor(self::MSG_FLASH);
+        $this->assertSelectorExists(self::MSG_FLASH.self::ALERT_SUCCESS);
+
+        $this->clickElement(self::BUTTON_CLOSE_MSG);
+        $this->clickElement(self::MODAL_BUTTON_CLOSE);
     }
 
-    private function editNote(Crawler $crawler): Crawler
+    private function editNote(): void
     {
         $this->outputMsg('Edit a note');
 
-        $this->client->waitForVisibility('#container-notes div[data-note-id]', 1);
-        sleep(1); //pop-up effect
+        $this->clickElement(self::CONTAINER_NOTES.' div[data-note-id]');
 
-        $this->client->waitForVisibility('#container-notes', 1);
-        $crawler->filter('#container-notes div[data-note-id]')->eq(1)->click();
-        sleep(1); //pop-up effect
+        $this->client->waitFor(self::FORM_NOTE);
+        sleep(1);
+        $this->client
+            ->getCrawler()
+            ->filter(self::FORM_NOTE)
+            ->form([
+                'note[title]' => $this->faker->sentence(mt_rand(5, 10), true),
+                'note[editor]' => join('. ', $this->faker->paragraphs(mt_rand(1, 2))),
+                'note[tags]' => [1, 2],
+            ])
+        ;
 
-        $this->client->waitForVisibility('button[data-action="save"]', 1);
-        $form = $crawler->filter('button[data-action="save"]')->form([
-            'note[title]' => $this->faker->sentence(mt_rand(5, 10), true),
-            'note[editor]' => join('. ', $this->faker->paragraphs(mt_rand(1, 2))),
-        ]);
+        $this->clickElement(self::MODAL_BUTTON_SAVE);
 
-        /** @var Crawler */
-        $crawler = $this->client->submit($form);
+        $this->client->waitFor(self::MSG_FLASH);
+        $this->assertSelectorExists(self::MSG_FLASH.self::ALERT_SUCCESS);
 
-        $this->client->waitFor('#js-msg-flash', 3);
-        $this->assertSelectorExists('#js-msg-flash.alert.alert-success');
-
-        $crawler->selectButton('btn-close-msg')->click();
-//        sleep(5);
-//        $crawler->filter('button[data-action="close"]')->click();
-//        $this->outputMsg('__ okok __');
-
-//        sleep(1);
-
-        return $crawler;
+        $this->clickElement(self::BUTTON_CLOSE_MSG);
+        $this->clickElement(self::MODAL_BUTTON_CLOSE);
     }
 
-    private function deleteNote(Crawler $crawler): Crawler
+    private function deleteNoteByModal(): void
     {
-        $this->outputMsg('Delete a note');
+        $this->outputMsg('Delete a note by modal');
 
-        $this->client->waitForVisibility('#container-notes div[data-note-id]');
-        $crawler->filter('#container-notes div[data-note-id]')->eq(1)->click();
-
-        sleep(1); //pop-up effect
-
-        $this->client->waitForVisibility('#modal-btn-delete');
-        $crawler->filter('#modal-btn-delete')->click();
-
+        $this->clickElement(self::CONTAINER_NOTES.' div[data-note-id]');
         sleep(1);
-        $this->client->waitForVisibility('#confirm-modal');
-
-        $crawler->filter('#modal-confirm-btn')->click();
-
-        $this->client->waitForVisibility('#js-msg-flash');
-        $this->assertSelectorExists('#js-msg-flash.alert.alert-warning');
-
-        $crawler->selectButton('btn-close-msg')->click();
-        // $crawler->filter('button[data-action="close"]')->click();
-
+        $this->client->waitFor('#confirm-modal');
+        $this->clickElement('#modal-btn-delete');
         sleep(1);
+        $this->clickElement('#modal-confirm-btn');
 
-        return $crawler;
+        $this->client->waitFor(self::MSG_FLASH);
+        $this->assertSelectorExists(self::MSG_FLASH.self::ALERT_WARNING);
+
+        $this->clickElement(self::BUTTON_CLOSE_MSG);
     }
 
-    private function restoreCardNote(Crawler $crawler): void
+    private function deleteNoteByTable(): void
+    {
+        $this->outputMsg('Delete a note by table');
+
+        $this->clickElement('a#table-view');
+        $this->clickElement(self::CONTAINER_NOTES.' tbody>tr button[data-action="delete-note"]');
+        sleep(1);
+        $this->clickElement('#modal-confirm');
+
+        $this->client->waitFor(self::MSG_FLASH.self::ALERT_WARNING);
+        $this->assertSelectorExists(self::MSG_FLASH.self::ALERT_WARNING);
+
+        $this->clickElement(self::BUTTON_CLOSE_MSG);
+    }
+
+    private function restoreCardNote(): void
     {
         $this->outputMsg('Restore a note from card view');
 
-        $crawler->filter('label[for="deleted_deleted"]')->click();
-        $crawler->filter('button[id="search"]')->click();
+        $this->clickElement('label[for="deleted_deleted"]');
+        $this->clickElement('button[id="search"]');
 
-        $this->client->waitFor('#container-notes', 1);
+        $this->client->waitFor(self::CONTAINER_NOTES);
 
         $this->restoreNote();
     }
@@ -187,21 +186,19 @@ class NoteEndToEndTest extends PantherTestCase
     {
         $this->outputMsg('Restore a note from table view');
 
-        $this->client->getWebDriver()->findElement(WebDriverBy::id('table-view'))->click();
-
         $this->clickElement('label[for="deleted_deleted"]');
         $this->clickElement('button[id="search"]');
 
-        $this->client->waitFor('table', 1);
+        $this->client->waitFor('table');
 
         $this->restoreNote();
     }
 
     private function restoreNote(): void
     {
-        $this->client->getWebDriver()->findElement(WebDriverBy::name('restore'))->click();
+        $this->clickElement('button[name="restore"]');
 
-        $this->client->waitFor('#js-msg-flash.alert', 3);
-        $this->assertSelectorExists('#js-msg-flash.alert.alert-success');
+        $this->client->waitFor(self::MSG_FLASH);
+        $this->assertSelectorExists(self::MSG_FLASH.self::ALERT_SUCCESS);
     }
 }
