1826f43043ac0afb63d16d07fe859b8451bdeb0f
RomMad <romain.madelaine@gmail.com>
2022-07-06 11:45:31 +0200

    Add maxlength for phone mask

--- assets/js/utils/maskPhone.js
+++ assets/js/utils/maskPhone.js
@@ -6,6 +6,7 @@ import 'cleave.js/dist/addons/cleave-phone.fr.js'
  */
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('input[data-phone]').forEach(inpuElt => {
+        inpuElt.setAttribute('maxlength', 14)
         new Cleave(inpuElt, {
             phone: true,
             phoneRegionCode: 'fr'
